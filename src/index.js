a38550774279e48e5700f14d59e023f31c0fd65f
Greg Rubin <grubin@catchpoint.com>
2017-05-02 16:07:42 -0400

    made a class for app

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 //Saving youtube API key
 const api_key = 'AIzaSyC6yr7WO9K626VdKJnspuL23HN8oJReFww';
 
-YTSearch({key: api_key, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
-
 // Create a new component. Should produce some HTML
-const App = () => {
-  return (<div>
+class App extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []};
+    YTSearch({key: api_key, term: 'surfboards'}, (videos) => {
+      this.setState({videos});
+    });
+  }
+  render() {
+    return (<div>
     <SearchBar />
   </div>
   );
+  }
 }
 
 //Take component's HTML and put it in the DOM
